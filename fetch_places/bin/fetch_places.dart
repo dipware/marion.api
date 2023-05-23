c99c694ac2bb246aa934e69af81db0191b0c0a8f
Matthew Dippel <mdip226@gmail.com>
2023-05-23 17:12:40 -0400

    first commit I think

---
+++ fetch_places/bin/fetch_places.dart
@@ -0,0 +1,6 @@
+// import 'package:fetch_places/fetch_places.dart' as fetch_places;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print(Platform.environment[])
+}
---
+++ fetch_places/test/fetch_places_test.dart
@@ -0,0 +1,8 @@
+import 'package:fetch_places/fetch_places.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
