f148dd5b37d9b23d22a7424fc91a95c2681d25c5
Matthew Dippel <mdip226@gmail.com>
2023-05-24 10:39:01 -0400

    draft

--- fetch_places/bin/fetch_places.dart
+++ fetch_places/bin/fetch_places.dart
@@ -1,5 +1,17 @@
 import 'package:fetch_places/fetch_places.dart';
 
-void main(List<String> arguments) {
-  print(Env.key1);
+void main(List<String> arguments) async {
+  if (arguments.isNotEmpty) {
+    final cmd = arguments[0];
+    switch (cmd) {
+      case 'init':
+        await call_nearby_places_api();
+        break;
+      case 'dev':
+        await dev();
+        break;
+      default:
+        break;
+    }
+  }
 }
